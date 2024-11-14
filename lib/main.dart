32c7a0b07dfef17e53c8b23f9ae3933b8aab735b
BRYSON <brysonminodi@gmail.com>
2024-11-14 09:29:22 +0300

    added bottom navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:practice/profile_screen.dart';
+import 'package:practice/settings_screen.dart';
 import 'package:practice/user_list_screen.dart';
 import 'package:practice/user_provider.dart';
 import 'package:practice/user_repository.dart';
@@ -24,7 +26,52 @@ class MyApp extends StatelessWidget {
         ),
       ],
       child: MaterialApp(
-        home: UserListScreen(),
+        home: MainScreen(),
+      ),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _currentIndex = 0;
+  final List<Widget> _screens = [
+    UserListScreen(),      // Home screen
+    SettingsScreen(),      // Settings screen
+    ProfileScreen(),       // Profile screen
+  ];
+
+  void _onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: _onTabTapped,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
       ),
     );
   }
